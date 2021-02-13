892901a2f4a87bae0f59e068c135f7f66cf1a1f2
EC2 Default User <ec2-user@ip-172-31-2-15.ap-south-1.compute.internal>
2021-02-13 07:11:03 +0000

    Codemirror Integration

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import 'codemirror/mode/javascript/javascript';
 
 if (environment.production) {
   enableProdMode();
